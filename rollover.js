f97b72830cb50e1c6cdbc39f83456e4b98621af4
Eric D Helms <ericdhelms@gmail.com>
2011-12-06 20:10:23 -0500

    Adds a check to see if tasks have been rolled over for the current day to reduce API calls.  The tasks will only rollover every 24 hours.  To account for the potential to miss the 24 hour window - a button has been added to the popup for the extension to allow manual rollover.

--- rollover.js
+++ rollover.js
@@ -13,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+var tasks_rolled_over = { rolled_over : false, timestamp : new Date(0) };
+
 function notifyRolledOver(){
     var logo = 'images/rollover-clock_48x48.png',
         title = 'RollOver Success',
@@ -21,47 +23,58 @@ function notifyRolledOver(){
     notify(logo, title, body);
 };
 
-var rollTasksOver = function(callback){
+var rollTasksOver = function(callback, manual){
     callback = callback || function(){};
+    manual = manual || false;
 
-    getTasks(function(data){
-        var length=data.items.length, 
-            i=0,
-            task,
-            taskDate,
-            today,
-            notify = false;
+    var today = new Date();
 
-        today = new Date();
-        
-        console.log('Checking for uncompleted tasks in the past')
-        for( i=0; i < length; i +=1 ){
-            task = data.items[i];
+    if( tasks_rolled_over.timestamp.getTime() + 24*60*60*1000 <= today.getTime() ){
+        tasks_rolled_over.rolled_over = false;
+        tasks_rolled_over.timestamp = today;
+    } 
+    
+    if( !tasks_rolled_over.rolled_over || manual ){
+        getTasks(function(data){
+            var length=data.items.length, 
+                i=0,
+                task,
+                taskDate,
+                notify = false;
+
+            console.log('Checking for uncompleted tasks in the past')
             
-            if( task.due && !task.completed ){
-                taskDate = new Date(task.due);
+            for( i=0; i < length; i +=1 ){
+                task = data.items[i];
+                
+                if( task.due && !task.completed ){
+                    taskDate = new Date(task.due);
 
-                if( ((taskDate.getDate() + 1) < today.getDate() && taskDate.getMonth() <= today.getMonth() && taskDate.getYear() <= today.getYear() ) || (taskDate.getMonth() < today.getMonth() && taskDate.getYear() <= today.getYear()) ){
-                    console.log('Rolling task over');
-                    taskDate.setMinutes(0);
-                    taskDate.setHours(0);
-                    taskDate.setSeconds(0);
-                    taskDate.setFullYear(today.getFullYear());
-                    taskDate.setDate(today.getDate());
-                    taskDate.setMonth(today.getMonth());
-                    task.due = taskDate.toISOString();
-                    updateTask({ 'due' : task.due, "id" : task.id });
-                    notify = true;
+                    if( ((taskDate.getDate() + 1) < today.getDate() && taskDate.getMonth() <= today.getMonth() && taskDate.getYear() <= today.getYear() ) || (taskDate.getMonth() < today.getMonth() && taskDate.getYear() <= today.getYear()) ){
+                        console.log('Rolling task over');
+                        taskDate.setMinutes(0);
+                        taskDate.setHours(0);
+                        taskDate.setSeconds(0);
+                        taskDate.setFullYear(today.getFullYear());
+                        taskDate.setDate(today.getDate());
+                        taskDate.setMonth(today.getMonth());
+                        task.due = taskDate.toISOString();
+                        updateTask({ 'due' : task.due, "id" : task.id });
+                        notify = true;
+                    }
                 }
             }
-        }
 
-        if( notify ){
-            notifyRolledOver();
-        }
-        
+            if( notify ){
+                tasks_rolled_over.rolled_over = true;
+                notifyRolledOver();
+            }
+            
+            callback();
+        });
+    } else {
         callback();
-    });
+    }
 };
 
 function checkForCalendarUrl(tabId, changeInfo, tab) {
